5bebf85672f9e325029f49e586c87943630cc34a
Ubuntu <daden@daden14V100.vkan4ijbfssend01x4eburxwdh.ux.internal.cloudapp.net>
2019-10-08 20:05:24 +0000

    make submodule installable

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name='BertSum',
     version='0.1',
-    packages=['bertsum'],
+    packages=find_packages(exclude=("bert_data","json_data", "logs","models", "raw_data", "results", "urls")),
     description='Fine-tune BERT for Extractive Summarization(https://arxiv.org/pdf/1903.10318.pdf)',
 )
 
