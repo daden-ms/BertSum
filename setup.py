b559be6221c7fa6d3b926216be101d456dbc70e1
Ubuntu <daden@daden14V100.vkan4ijbfssend01x4eburxwdh.ux.internal.cloudapp.net>
2019-10-08 16:56:19 +0000

    make the source installable

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name='BertSum',
+    version='0.1',
+    packages=['bertsum'],
+    description='Fine-tune BERT for Extractive Summarization(https://arxiv.org/pdf/1903.10318.pdf)',
+)
+
