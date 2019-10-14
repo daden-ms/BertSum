cd426b990324a71bd3a13599d911600769f9b975
Ubuntu <daden@daden14V100.vkan4ijbfssend01x4eburxwdh.ux.internal.cloudapp.net>
2019-10-14 18:43:38 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 setup(
     name='BertSum',
     version='0.1',
-    packages=find_packages(exclude=("bert_data","json_data", "logs","models", "raw_data", "results", "urls")),
+    author='Yang Liu',
+    author_email='https://github.com/daden-ms/BertSum',
+    url='https://github.com/daden-ms/BertSum', 
     description='Fine-tune BERT for Extractive Summarization(https://arxiv.org/pdf/1903.10318.pdf)',
+    packages=find_packages(exclude=("bert_data","json_data", "logs","models", "raw_data", "results", "urls")),
 )
 
